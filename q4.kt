89d2ad14303e1e18ff34e92903aa7d5c80206fe9
Ssubhams19 <subhamsahoo02195@gmail.com>
2026-05-28 14:42:10 +0530

    Add function to reverse a number in q4.kt

---
+++ q4.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var num = 1234
+    var reverse = 0
+
+    while (num != 0) {
+        val digit = num % 10
+        reverse = reverse * 10 + digit
+        num /= 10
+    }
+
+    println(reverse)
+}
