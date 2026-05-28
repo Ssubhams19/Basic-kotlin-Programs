423eda569576c5a4e9c1b4158e457a149268899e
Ssubhams19 <subhamsahoo02195@gmail.com>
2026-05-28 14:50:04 +0530

    Add function to check for vowels and consonants

---
+++ q10.kt
@@ -0,0 +1,11 @@
+fun check(ch: Char) {
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        println("Vowel")
+    } else {
+        println("Consonant")
+    }
+}
+
+fun main() {
+    check('a')
+}
